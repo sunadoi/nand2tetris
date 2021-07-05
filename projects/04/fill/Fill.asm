7c4a7e8a8f6a7000ea2d5f3d8bcacdde6838c4a8
sunadoi <y.sunadoi@gmail.com>
2021-07-05 23:53:57 +0900

    04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,75 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	// R0: if keyboard is pushed in previous term then 1
+  // R1: if keyboard is pushed in current term then 1
+
+  @8192 // screen size 32*256
+  D=A
+  @number_of_pixel
+  M=D // number_of_pixel=8192
+  @R0
+  M=0 // init
+(LOOP)
+  @KBD
+  D=M
+  @KEY_PUSHED
+  D;JNE // if KBD != 0 goto KEY_PUSHED
+(KEY_NOT_PUSHED)
+  @R1
+  M=0
+  @CHECK_STATE_CHANGE
+  0;JMP
+(KEY_PUSHED)
+  @R1
+  M=1
+(CHECK_STATE_CHANGE)
+  @R0
+  D=M
+  @R1
+  D=D-M // R0-R1
+  @LOOP
+  D;JEQ   // if D == 0 goto LOOP 入力に変化なし
+
+  @R1
+  D=M // D=R1
+  @R0
+  M=D // R0=R1 状態の更新
+
+  @SET_WHITE
+  D;JEQ // if D == 0 goto WHITE_LOOP
+(SET_BLACK)
+  @color
+  M=-1 // color=-1
+  @INIT_POSITION
+  0;JMP
+(SET_WHITE)
+  @color
+  M=0 // color=0
+(INIT_POSITION)
+  @SCREEN
+  D=A // D=16384
+  @position
+  M=D
+  @count
+  M=0
+(SCREEN_LOOP)
+  @count
+  D=M
+  @number_of_pixel
+  D=M-D // D=number_of_pixel - count
+  @LOOP
+  D;JLE // if D <= 0 goto LOOP 終了条件
+
+  @color
+  D=M
+  @position
+  A=M
+  M=D
+
+  @position
+  M=M+1
+  @count
+  M=M+1
+  @SCREEN_LOOP
+  0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,25 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+      @R2
+      M=0 // R2=0
+      @i
+      M=0 // i=0
+(LOOP)
+      @R1
+      D=M // D=R1
+      @i
+      D=D-M // D=R1-i
+      @END
+      D;JLE // IF (R1-i) <= 0 ならENDへ
+      @R0
+      D=M // D=R0
+      @R2
+      M=D+M // R2 += R0
+      @i
+      M=M+1 // i++
+      @LOOP
+      0;JMP
+(END)
+      @END
+      0;JMP
